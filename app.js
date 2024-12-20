27388bd70a7fcc57521cb999a07f62b9281e14df
Danpixels <danypixls66@gmail.com>
2024-12-19 22:15:41 -0300

    Jogo atualizado e com mais funções

--- app.js
+++ app.js
@@ -1,18 +1,51 @@
-function gerarNumero (){
-     return parseInt(Math.random () * 10 + 1);
+let numeroSecreto = gerarNumero();
+let tentativas = 1;
+
+exibirMensagem();
+
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+
+    if (chute == numeroSecreto) {
+        alterarTexto('h1', 'Acertou');
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}`
+        alterarTexto('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        if (chute > numeroSecreto) {
+            alterarTexto('p', 'o número secreto é menor');
+        } else {
+            alterarTexto('p', 'o número secreto é maior');
+        }
+        tentativas++;
+        limparCampo();
+    }
 }
 
-let numeroSecreto = gerarNumero();
+function gerarNumero () {
+    return parseInt(Math.random () * 10 + 1);
+}
 
 function alterarTexto (tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
 
-alterarTexto ('h1', 'Jogo do número secreto');
-alterarTexto ('p', 'Escolha um número entre 1 e 10');
+function limparCampo() {
+    chute = document.querySelector('input');
+    chute.value = '';
+}
 
-function verificarChute() {
-    let chute = document.querySelector('input').value;
-    console.log(chute == numeroSecreto);
+function exibirMensagem () {
+    alterarTexto ('h1', 'Jogo do número secreto');
+    alterarTexto ('p', 'Escolha um número entre 1 e 10');
+}
+
+function reiniciarJogo() {
+    numeroSecreto = gerarNumero();
+    limparCampo();
+    tentativas = 1;
+    exibirMensagem();
+    document.getElementById('reiniciar').setAttribute('disabled', true);
 }
